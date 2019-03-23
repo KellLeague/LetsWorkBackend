82ade7bcfae61e180e9bcda47d9d331e3c002b66
Ivy Strickland <IvyStrickland@pursuit.org>
2019-03-22 23:24:12 -0400

    users and post

---
+++ seed.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS letswork;
+CREATE DATABASE letswork;
+
+\c letswork;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY NOT NULL,
+    username VARCHAR UNIQUE NOT NULL,
+    email VARCHAR UNIQUE NOT NULL,
+    avatar VARCHAR NOT NULL,
+    created TIMESTAMP DEFAULT NOW(),
+    updated TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE posts (
+    user_id INT REFERENCES users(id) ,
+    caption VARCHAR NOT NULL,
+    likes VARCHAR NOT NULL,
+    comments VARCHAR NOT NULL,
+    created TIMESTAMP DEFAULT NOW(),
+    updated TIMESTAMP DEFAULT NOW()
+);
+
+-- CREATE TABLE comments (
+--     user_id INT REFERENCES users(id),
+--     comment VARCHAR NOT NULL,
+--     created TIMESTAMP DEFAULT NOW(),
+--     updated TIMESTAMP DEFAULT NOW()
+-- )
+
+INSERT INTO users
+(username, email, avatar) VALUES
+('userone', 'userone@email.com', 'https://ca.slack-edge.com/TD416AWAE-UD52WKQPR-05976bd60d01-48');
+
+INSERT INTO posts
+(caption, likes, comments)
+VALUES
+('poppin gz','62783871','hey jo');
+
+-- INSERT INTO comments
+-- (comment)
+-- VALUES
+-- ('usertext');
